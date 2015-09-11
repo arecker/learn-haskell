70a37b3bc8e73ce2bf5d3c99a37a890e900285af
Alex Recker <alex@reckerfamily.com>
2015-09-10 21:51:34 -0500

    Adds "Starting Out" lesson

---
+++ startingOut.hs
@@ -0,0 +1,17 @@
+doubleMe x = x * 2
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                      then x
+                      else doubleMe x
+
+doubleSmallNumber' x = (if x > 100 then x else doubleMe x)
+
+conanO'Brien = "It's a-me, Conan O'Brien"
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+
+length' xs = sum [ 1 | _ <- xs ]
+
+removeNonUppercase st = [ l | l <- st, l `elem` ['A'..'Z'] ]
